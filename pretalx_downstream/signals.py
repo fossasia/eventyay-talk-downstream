f9658d22ab73ef8de4627e9fd4e400ecabbc8e73
Tobias Kunze <r@rixx.de>
2018-12-07 00:48:33 +0100

    Only retrieve schedule if the interval is up

--- pretalx_downstream/signals.py
+++ pretalx_downstream/signals.py
@@ -13,7 +13,10 @@ from .tasks import task_refresh_upstream_schedule
 
 @receiver(periodic_task)
 def refresh_upstream_schedule(sender, request, **kwargs):
-    task_refresh_upstream_schedule.apply_async(sender)
+    interval = timedelta(minutes=sender.settings.downstream_interval or 5)
+    last_pulled = sender.settings.downstream_last_sync
+    if not last_pulled or now() - last_pulled > interval:
+        task_refresh_upstream_schedule.apply_async(sender)
 
 
 @receiver(nav_event_settings)
