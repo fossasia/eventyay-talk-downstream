bc8b6fde44f44f3d0592820ecc78d9c8ad6253b2
Tobias Kunze <r@rixx.de>
2018-12-06 23:34:41 +0100

    Initial commit

---
+++ pretalx_downstream/__init__.py
@@ -0,0 +1,20 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy
+
+
+class PluginApp(AppConfig):
+    name = 'pretalx_downstream'
+    verbose_name = 'pretalx as a downstream service'
+
+    class PretalxPluginMeta:
+        name = ugettext_lazy('pretalx as a downstream service')
+        author = 'Tobias Kunze'
+        description = ugettext_lazy('This plugin allows you to use pretalx passively, by letting it import another event's schedule.')
+        visible = True
+        version = '0.0.0'
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = 'pretalx_downstream.PluginApp'
---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+from distutils.command.build import build
+
+from django.core import management
+from setuptools import setup, find_packages
+
+
+try:
+    with open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8') as f:
+        long_description = f.read()
+except Exception:
+    long_description = ''
+
+
+class CustomBuild(build):
+    def run(self):
+        management.call_command('compilemessages', verbosity=1, interactive=False)
+        build.run(self)
+
+
+cmdclass = {
+    'build': CustomBuild
+}
+
+
+setup(
+    name='pretalx-downstream',
+    version='0.0.0',
+    description='This plugin allows you to use pretalx passively, by letting it import another event\'s schedule.',
+    long_description=long_description,
+    url='https://github.com/pretalx/pretalx-downstream',
+    author='Tobias Kunze',
+    author_email='r@rixx.de',
+    license='Apache Software License',
+
+    install_requires=[],
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    cmdclass=cmdclass,
+    entry_points="""
+[pretalx.plugin]
+pretalx_downstream=pretalx_downstream:PretalxPluginMeta
+""",
+)
