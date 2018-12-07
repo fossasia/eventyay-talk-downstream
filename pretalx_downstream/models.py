dc689ba0c42d5beeb0523c94d2cda022705adaf2
Tobias Kunze <r@rixx.de>
2018-12-07 01:32:34 +0100

    Add data model for storing received data

---
+++ pretalx_downstream/models.py
@@ -0,0 +1,28 @@
+import hashlib
+
+from django.db import models
+
+
+class UpstreamResult(models.Model):
+    event = models.ForeignKey(
+        to='event.Event', on_delete=models.CASCADE, related_name='upstream_results'
+    )
+    schedule = models.ForeignKey(
+        to='schedule.Schedule',
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='upstream_results',
+    )
+    content = models.TextField(null=True, blank=True)
+    changes = models.TextField(
+        null=True, blank=True
+    )  # contains only content changes, all regular changes will be showin in the related schedule update (if any)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def checksum(self):
+        if not self.content:
+            return None
+        m = hashlib.sha256()
+        m.update(self.content.encode('utf-8'))
+        return h.hexdigest()
