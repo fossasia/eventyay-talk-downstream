a039ca18a0cc37b266c9b8966a9a318ad40bc0e3
Tobias Kunze <r@rixx.de>
2018-12-18 00:04:35 +0100

    Periodic tasks are not per-event

--- pretalx_downstream/signals.py
+++ pretalx_downstream/signals.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from django.utils.timezone import now
 from django.utils.translation import ugettext_lazy as _
 from pretalx.common.signals import periodic_task
+from pretalx.event.models import Event
 from pretalx.orga.signals import nav_event_settings
 
 from .tasks import task_refresh_upstream_schedule
@@ -12,10 +13,12 @@ from .tasks import task_refresh_upstream_schedule
 
 @receiver(periodic_task)
 def refresh_upstream_schedule(sender, request=None, **kwargs):
-    interval = timedelta(minutes=sender.settings.downstream_interval or 5)
-    last_pulled = sender.settings.downstream_last_sync
-    if not last_pulled or now() - last_pulled > interval:
-        task_refresh_upstream_schedule.apply_async(sender.slug)
+    for event in Event.objects.all():
+        if event.settings.downstream_upstream_url:
+            interval = timedelta(minutes=event.settings.downstream_interval or 5)
+            last_pulled = event.settings.downstream_last_sync
+            if not last_pulled or now() - last_pulled > interval:
+                task_refresh_upstream_schedule.apply_async(event.slug)
 
 
 @receiver(nav_event_settings)
