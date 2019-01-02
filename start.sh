b817dd5eb00ebb43021f1ec1ce20a75098e50ff8
Daniel Goodman <daniel@goodes.net>
2019-01-02 20:12:45 +0200

    updating start.sh to address install issues
    - adding sudo to chown to address permissions issue
    - starting DB 10 secs before final up to give it time to start

--- start.sh
+++ start.sh
@@ -16,16 +16,19 @@ mkdir -p volumes/mysql-data && \
 mkdir -p volumes/nginx-data && \
 mkdir -p volumes/pretalx-data && \
 mkdir -p volumes/redis-data && \
-chown -R 999:999 volumes && \
+sudo chown -R 999:999 volumes && \
 MYSQL_PASSWORD=`LC_CTYPE=C tr -dc 'a-zA-Z0-9'<  /dev/urandom | fold -w 30 | head -1`
 echo Generated mysql root password: $MYSQL_PASSWORD && \
-echo $MYSQL_PASSWORD > conf/mysql-password.secret && \
+echo "$MYSQL_PASSWORD" > conf/mysql-password.secret && \
 echo Initializing mysql... && \
 docker-compose up -d db  && \
 echo Waiting 30s to make sure mysql is configured... && \
 sleep 30 && \
 docker-compose down && \
 echo Starting pretalx_core pretalx_celery pretalx_mysql pretalx_nginx pretalx_smtp && \
+docker-compose up -d db  && \
+echo Waiting 10 to make sure mysql is configured... && \
+sleep 10 && \
 docker-compose up -d && \
 echo Waiting 30s to make sure everything boots up... && \
 sleep 30 && \
