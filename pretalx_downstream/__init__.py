6331332411fc486725a99991cd7b6652f2cf9d35
Tobias Kunze <r@rixx.de>
2020-12-27 20:18:03 +0100

    Release v0.9.1

--- pretalx_downstream/__init__.py
+++ pretalx_downstream/__init__.py
@@ -13,7 +13,7 @@ class PluginApp(AppConfig):
             "This plugin allows you to use pretalx passively, by letting it import another event's schedule."
         )
         visible = True
-        version = "0.9.0"
+        version = "0.9.1"
 
     def ready(self):
         from . import signals  # NOQA
