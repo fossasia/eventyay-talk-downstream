9f218af851a9c970103a6e76683b860e9fbf0212
Tobias Kunze <r@rixx.de>
2024-02-13 11:04:13 +0100

    Use SLUG_REGEX instead of SLUG_CHARS

--- pretalx_downstream/urls.py
+++ pretalx_downstream/urls.py
@@ -1,11 +1,11 @@
 from django.urls import re_path
-from pretalx.event.models.event import SLUG_CHARS
+from pretalx.event.models.event import SLUG_REGEX
 
 from .views import UpstreamSettings
 
 urlpatterns = [
     re_path(
-        rf"^orga/event/(?P<event>[{SLUG_CHARS}]+)/settings/p/upstream/$",
+        rf"^orga/event/(?P<event>{SLUG_REGEX})/settings/p/upstream/$",
         UpstreamSettings.as_view(),
         name="settings",
     )
