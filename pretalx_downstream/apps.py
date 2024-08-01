329649f655a036ee8859e716b846f4792a27280f
odkhang <odkhang@tma.com.vn>
2024-08-01 16:46:30 +0700

    update plugin compability with current eventyay-talk (#7)
    Co-authored-by: lcduong <lcduong@tma.com.vn>

--- pretalx_downstream/apps.py
+++ pretalx_downstream/apps.py
@@ -6,13 +6,13 @@ from pretalx_downstream import __version__
 
 class PluginApp(AppConfig):
     name = "pretalx_downstream"
-    verbose_name = "pretalx as a downstream service"
+    verbose_name = "eventyay-talk as a downstream service"
 
     class PretalxPluginMeta:
         name = gettext_lazy("pretalx as a downstream service")
         author = "Tobias Kunze"
         description = gettext_lazy(
-            "This plugin allows you to use pretalx passively, by letting it import another event's schedule."
+            "This plugin allows you to use eventyay-talk passively, by letting it import another event's schedule."
         )
         visible = True
         version = __version__
